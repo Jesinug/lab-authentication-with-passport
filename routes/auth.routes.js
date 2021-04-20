a1854c309f67b2883985d77fceddcd43c4aa6d3e
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-20 16:40:28 +0200

    Iteration 1 work in progress

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,62 @@
 const express = require('express');
 const router = express.Router();
-// Require user model
-
-// Add bcrypt to encrypt passwords
-
-// Add passport
-
+const user = require('../models/User.model')
+const bcrypt = require('bcryptjs')
+const passport = require('passport');
+const saltRounds = 10;
 const ensureLogin = require('connect-ensure-login');
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+//
+
+
+const isLoggedOut = (req, res, next) => {
+  if(req.authenticated()) {
+    res.redirect('/private/profile');
+  } else { next() };
+}
+
+router.get('/signup', isLoggedOut, (req, res) => {
+  res.render('signup')
+}) 
+
+router.post('signup', (req, res) => {
+  const { username, password } = req.body;
+  if(!username || !password) {
+    res.render('signup', {errorMessage: "Username and password are required"})
+  }
+  User.findOne({ username })
+  .then(user => {
+    if(user) {
+      res.render('signup', {errorMessage: "Username already exists"})  
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashPass = bcrypt.hashSync(password, salt);
+    
+    User.create({ username, password: hashPass})
+    .then((newUser) => {
+      req.login(newUser, (error) => {
+        if(error) {
+          next(error)
+        } 
+        return res.redirect('/private/profile')
+      })
+    })
+  })
+  .catch(error) => {
+    (console.log(error))
+    return res.render('signup', {errorMessage: "Server error. Try again"})
+    }
+})
+
+  router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('passport/private', { user: req.user });
 });
+
+/* Middleware
+isLoggedOut
+isLoggedIn
+*/
+
+
 
 module.exports = router;
